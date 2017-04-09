0dc83077427dfe6c82349ac35ac8135ecee6de52
Nicolas Wielonsky <nicolas.wielonsky@gmail.com>
2017-04-09 19:12:01 +0200

    Change UI, add json file

--- app/initialize.js
+++ app/initialize.js
@@ -3,32 +3,38 @@ import './js/helpers';
 
 $(document).ready(function() {
    let jsonLocal = {};
-   $.get('https://jsonblob.com/api/e1e7fb29-125b-11e7-a0ba-17d4fd61f6e6', function(data) {
-      jsonLocal = data;
+   let jsonThemeLocal = {};
+   $.get('data/themes.json', function(data) {
       Object.keys(data).forEach((key) => {
-         $('#sidebar').append(
-            `
-            <a class="panel-block panel-event" id="panel-block-${key.split(' ').pop()}">
+         data[key].forEach((item) => {
+            jsonThemeLocal[item.tag] = item;
+            $('#sidebar').append(
+               `
+            <a class="panel-event" id="panel-block-${item.tag
+                  .split(' ')
+                  .pop()}" data-key="${item.tag}">
                <span class="panel-img">
-                  <img class="circular--square" src="http://placehold.it/150x150" alt=""/>
-               </span> 
-               <span id="panel-key">${key}</span>
+                  <img class="circular--square" src="${item.picture}" alt=""/>
+               </span>
             </a>
             `,
-         );
+            );
+         });
       });
-      appendVideo(Object.keys(data)[0], 4);
-      appendPresentation(Object.keys(data)[0]);
    });
+   $.get(
+      'https://jsonblob.com/api/e1e7fb29-125b-11e7-a0ba-17d4fd61f6e6',
+      function(data) {
+         jsonLocal = data;
+         appendVideo(Object.keys(data)[0]);
+         appendPresentation(Object.keys(data)[0]);
+      },
+   );
 
    $(document).on('click', '.panel-event', function() {
-      const key = $(this).children('#panel-key').text();
+      const key = $(this).data('key');
       appendPresentation(key);
-      appendVideo(key, $('#depth').val());
-   });
-
-   $('#depth').on('change', (event) => {
-      appendVideo($('#presentation-title').text(), event.target.value);
+      appendVideo(key);
    });
 
    function appendPresentation(key) {
@@ -36,12 +42,10 @@ $(document).ready(function() {
       $('#presentation').append(
          `
          <div class="columns">
-            <div class="column is-2">
-               <img class="circular--square level-item" src="http://placehold.it/150x150" alt=""/>
-            </div>
-            <div class="column is-10">
-                  <h1 class="title" id="presentation-title">${key}</h1><br>
-                  <h2 class="subtitle">Top 20 des vidéos les plus sugérées par Youtube.</h2>
+            <div class="column is-8 is-offset-2 has-text-centered">
+                  <img class="circular--square is-block" src="${jsonThemeLocal[key].picture}" alt=""/>
+                  <h1 class="title" id="presentation-title">Vidéos les plus suggérées par YouTube</h1><br>
+                  <h2 class="subtitle">dans la liste de lecture à droite à partir de la recherche "<a href="https://www.youtube.com/results?search_query=${key}">${key}</a>"</h2>
             </div>
          </div>
          `,
@@ -49,36 +53,40 @@ $(document).ready(function() {
    }
 
    // Permet d'actualiser les vidéos correspond au theme "key".
-   function appendVideo(key, depth) {
-      [].forEach.call($('.panel-block'), (item) => {
-         $(item).removeClass('is-active');
+   function appendVideo(key) {
+      $('#sidebar').children().each(function() {
+         $(this).removeClass('is-active');
       });
       $('#panel-block-' + key.split(' ').pop()).addClass('is-active');
       $('.videos').empty();
-      jsonLocal[key].filter((item) => item.likes !== -1 && item.depth >= depth).forEach((item, index) => {
-         $('.videos').append(
-            `
+      jsonLocal[key]
+         .filter((item) => item.likes !== -1)
+         .forEach((item, index) => {
+            if (index > 19) return;
+            $('.videos').append(
+               `
             <div class="box">
             <article class="media">
-               <figure class="media-left">
-               <a href="https://www.youtube.com/watch?v=${item.id}">
-                  <p class="image is-128x128">
-                     <img src="https://img.youtube.com/vi/${item.id}/default.jpg">
-                  </p>
-               </a>
+               <figure class="media-left level">
+                  <small class="level-item">${index + 1}</small>
+                  <a href="https://www.youtube.com/watch?v=${item.id}">
+                     <div class="is-inline-block level-item">
+                        <img class="image" width="170px" src="https://img.youtube.com/vi/${item.id}/hqdefault.jpg">
+                     </div>
+                  </a>
                </figure>
                <div class="media-content">
                   <div class="content">
                      <p>
                      <a href="https://www.youtube.com/watch?v=${item.id}"><strong class="video-title">${item.title}</strong></a><br>
-                     <span class="tag ${item.title.toLowerCase().latinise().includes(key.split(' ').pop().toLowerCase().latinise()) ? 'is-success' : 'is-primary'}">Suggéré ${item.recommendations} fois</span> <small>${item.views} vues - ${item.likes} likes - ${item.dislikes} unlikes</small><br>
+                  <small>${item.views} vues &nbsp; &nbsp;<i class="fa fa-thumbs-up" aria-hidden="true"></i>${item.likes}  <i class="fa fa-thumbs-down" aria-hidden="true"></i>${item.dislikes}</small><br>
                      </p>
                   </div>
                </div>
                </article></div>
 
          `,
-         );
-      });
+            );
+         });
    }
 });
