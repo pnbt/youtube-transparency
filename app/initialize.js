1dc8ebae8c6e5ba71cc34bc89d58f89c2c3aeb34
Nicolas Wielonsky <nicolas.wielonsky@gmail.com>
2017-04-15 17:19:40 +0200

    Fixes

--- app/initialize.js
+++ app/initialize.js
@@ -5,7 +5,7 @@ import './js/helpers';
 $(document).ready(function() {
    let jsonLocal = {};
    let jsonThemeLocal = {};
-   const lastDate = 'ytrecos-presidentielle-2017-04-12';
+   const lastDate = 'ytrecos-presidentielle-2017-04-15';
    $.get('data/themes.json', function(data) {
       Object.keys(data).forEach((key) => {
          data[key].forEach((item) => {
