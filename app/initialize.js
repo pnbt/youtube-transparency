f07fe034b6916a8cbc7fad79aa2c1b54198dc51b
Frédéric Bardolle <seiteta@users.noreply.github.com>
2017-04-27 15:00:12 +0200

    Add 2 days of data

--- app/initialize.js
+++ app/initialize.js
@@ -5,7 +5,7 @@ import './js/helpers';
 $(document).ready(function() {
    let jsonLocal = {};
    let jsonThemeLocal = {};
-   const lastDate = 'ytrecos-presidentielle-2017-04-22';
+   const lastDate = 'ytrecos-presidentielle-2017-04-24';
    $.get('data/themes.json', function(data) {
       Object.keys(data).forEach((key) => {
          data[key].forEach((item) => {
