c977c414c5f23c518a28d443b79669a6261b03bd
Guillaume Chaslot <guillaume@bayesimpact.org>
2017-05-12 16:15:11 +0200

    Update UK and FR.

--- app/initialize-uk.js
+++ app/initialize-uk.js
@@ -4,7 +4,7 @@ import './js/helpers';
 $(document).ready(function() {
    let jsonLocal = {};
    const jsonThemeLocal = {};
-   const lastDate = 'ytrecos-electionuk-2017-05-09';
+   const lastDate = 'ytrecos-electionuk-2017-05-12';
    $.get('/data/themesuk.json', function(data) {
       Object.keys(data).forEach((key) => {
          data[key].forEach((item) => {
