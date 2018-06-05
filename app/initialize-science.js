0484c9012292881e0e116d6ffce31bbeec6d32a3
Guillaume Chaslot <guillaume@bayesimpact.org>
2018-06-05 13:25:57 +0200

    June push.

--- app/initialize-science.js
+++ app/initialize-science.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
 
   let jsonLocal = {};
    const jsonThemeLocal = {};
-   const lastDate = 'ytrecos-science-2018-05-02';
+   const lastDate = 'ytrecos-science-2018-06-01';
    $.get('/data/themesscience.json', function(data) {
       Object.keys(data).forEach((key) => {
          data[key].forEach((item) => {
