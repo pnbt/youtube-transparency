9d2b30e224e0f35340c63ce98cc3586d26c090e9
Guillaume Chaslot <guillaume@bayesimpact.org>
2018-09-12 15:08:52 -0700

    Test

--- app/initialize-sweden.js
+++ app/initialize-sweden.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
    let jsonLocal = {};
    const jsonThemeLocal = {};
    const lastDate = 'ytrecos-election-sweden-2018-09-09';
+
    $.get('/data/themessweden.json', function(data) {
       Object.keys(data).forEach((key) => {
          console.log('DATA')
