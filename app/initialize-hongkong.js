953aee9ca07404b1488f350435ed50704ed8a070
Guillaume Chaslot <guillaume@bayesimpact.org>
2019-09-06 12:14:21 +0200

    Fix hong kong

--- app/initialize-hongkong.js
+++ app/initialize-hongkong.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
 
    let jsonLocal = {};
    const jsonThemeLocal = {};
-   const lastDate = 'ytrecos-hongkong-2019-09-06';
+   const lastDate = 'ytrecos-hongkong-2019-09-01';
    $.get('/data/themeshongkong.json', function(data) {
       Object.keys(data).forEach((key) => {
          data[key].forEach((item) => {
